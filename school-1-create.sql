f6b7d8352b68f5ba5ce1f24562515aba4f9587ec
Marko Jelasity <marko.jelasity@kellogg.northwestern.edu>
2025-01-21 20:13:53 +0000

    Session 2

--- school-1-create.sql
+++ school-1-create.sql
@@ -4,4 +4,5 @@ CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
   last_name TEXT
+  
 );
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students 
+SET last_name = "Kaur"
+WHERE id = 2;
